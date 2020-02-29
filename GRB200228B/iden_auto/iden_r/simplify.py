f289bf11492237f671bd5fbdd0e386c526d7f96c
wa1kmoon <DW445360071@163.com>
2020-02-29 14:38:49 +0800

    renew
    Signed-off-by: wa1kmoon <DW445360071@163.com>

--- GRB200228B/iden_auto/iden_r/simplify.py
+++ GRB200228B/iden_auto/iden_r/simplify.py
@@ -8,6 +8,11 @@ for i in range(len(f)):
     sublist.append(f[i][23])
     sublist.append(f[i][24])
     sublist.append(f[i][61])
+    sublist.append(f[i][62])
+    sublist.append(f[i][67])
+    sublist.append(f[i][68])
+    sublist.append(f[i][71])
+    sublist.append(f[i][72])
     list.append(sublist)
 
 b=open('simple_list.csv','w+')
